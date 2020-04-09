0f992aeeb67ea7926edbda84184987411a0b0f6a
VCD <VeryCrazyDog@gmail.com>
2020-04-09 20:53:09 +0800

    Fix lint errors

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <h1><a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">Conway's Game of Life</a></h1>
-    <GameWorld/>
+    <h1>
+      <a
+        href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+        target="_blank"
+      >Conway's Game of Life</a>
+    </h1>
+    <GameWorld />
   </div>
 </template>
 
