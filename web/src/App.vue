af497990a796c781668c54d03cfef80e28dfaa4e
VCD <VeryCrazyDog@gmail.com>
2020-04-13 12:33:54 +0800

    Merge GameScreen.vue to App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,22 +7,49 @@
       >Conway's Game of Life</a>
     </h1>
     <ToolboxSidebar position="left" />
-    <GameScreen />
+    <div class="main">
+      <World
+        :size="world.size"
+        :are-cells-clickable="true"
+        @cell-clicked="cellClicked"
+      />
+    </div>
     <StatusSidebar position="right" />
   </div>
 </template>
 
 <script>
 import ToolboxSidebar from './components/ToolboxSidebar.vue'
-import GameScreen from './components/GameScreen.vue'
+import World from './components/World.vue'
 import StatusSidebar from './components/StatusSidebar.vue'
 
 export default {
   name: 'App',
   components: {
     ToolboxSidebar,
-    GameScreen,
+    World,
     StatusSidebar
+  },
+  data: function () {
+    return {
+      world: {
+        size: { x: 0, y: 0 },
+        cellList: []
+      }
+    }
+  },
+  methods: {
+    cellClicked: function (message) {
+      // TODO Implement
+      console.log('app', message)
+    }
+  },
+  sockets: {
+    'game start info': function (data) {
+      this.world.size = { x: data.world.xLength, y: data.world.yLength }
+      // updateWorld($world, data.world.cellList, { overwrite: true })
+      // hookWorld($world, socket, data.player.color)
+    }
   }
 }
 </script>
@@ -40,5 +67,9 @@ a {
 a:hover {
   color: lightgoldenrodyellow;
   text-decoration: underline;
+}
+
+div.main {
+  margin: 0 98px;
 }
 </style>
